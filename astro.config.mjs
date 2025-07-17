16b1f5d3343c9262a39ce17dc9e0797281ac9e10
drmooo <40821522+drmooo@users.noreply.github.com>
2025-07-17 12:37:36 +0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://vojtastruhar.github.io/",
+  base: "/steam-review-template",
+  integrations: [tailwind(), sitemap()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ar"],
+  }
+});
---
+++ src/review_templates/general.js
@@ -0,0 +1,7 @@
+import english_template from "./template_en.json"
+import arabic_template from "./template_ar.json"
+
+export default {
+  ar: arabic_template,
+  en: english_template
+}
